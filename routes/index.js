da1083d60b0e33f776b62a7cfa5494b78fb3e83b
centillion-303 <aldana.abdullah1@gmail.com>
2020-03-23 11:02:05 +0100

    fixied status code

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  res.status(200)
   console.log("Status:200 (Der Server funktioniert)")
 });
 
